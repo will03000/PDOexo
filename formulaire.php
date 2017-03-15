dba69aa48cb6e7d3f705d72d65b686522c5c7871
William <william.legal03@gmail.com>
2017-03-15 11:54:21 +0100

    exoform

--- formulaire.php
+++ formulaire.php
@@ -2,45 +2,46 @@
 $erreur = [];
 if (isset($_POST) && !empty($_POST)) {
 	
+	$donnee = [];
 
 	if (isset($_POST['lastName']) && $_POST['lastName'] !== ''){
-		$nom = $_POST['lastName'];
+		$donnee['lastName'] = $_POST['lastName'];
 	}else{
 		$erreur[] = 'merci de mettre un nom';
 	}
 	if (isset($_POST['firstName']) && $_POST['firstName'] !== '') {
-		$prenom = $_POST['firstName'];
+		$donnee['firstName'] = $_POST['firstName'];
 	}else{
 		$erreur[] = 'merci de mettre un prenom';
 	}
 	if (isset($_POST['birthDate'])) {
-			$naissance = $_POST['birthDate'];
+			$donnee['birthDate'] = $_POST['birthDate'];
 	}else{
 		$erreur[] = 'merci de mettre une date de naissance';
 	}
 	if (isset($_POST['card'])) {
-			$card = 1;
+			$donnee['card'] = 1;
 		
 		 if(isset($_POST['cardNumber'])) {
-		 	$numcarte = $_POST['cardNumber'];
+		 	$donnee['cardNumber'] = $_POST['cardNumber'];
 		}else{
 			$erreur[] = 'merci de mettre votre numéro de carte';
 		}
 	}else{
-		$card = 0;
-		$numcarte = null;
+		$donnee['card'] = 0;
+		$donnee['cardNumber'] = null;
 	}
 	if (empty($erreur)) {
 		$pdo = new PDO('mysql:dbname=colyseum;host=localhost;charset=utf8','root', '');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 		$statement = $pdo->prepare('INSERT INTO clients 
-									SET lastName = ?,
-										firstName = ?,
-										birthDate = ?,
-										card = ?,
-										cardNumber = ?');
-		$statement->execute([$nom,$prenom,$naissance,$card,$numcarte]);
+									SET lastName = :lastName,
+										firstName = :firstName,
+										birthDate = :birthDate,
+										card = :card,
+										cardNumber = :cardNumber');
+		$statement->execute($donnee);
 
 		$erreur[] = 'le client est bien ajouté';
 	}
