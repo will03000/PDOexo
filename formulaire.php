a7b2d4261f0ab80e3f57eb19fdf2f73ddc4297a7
William <william.legal03@gmail.com>
2017-03-15 09:37:40 +0100

    exoformulairedébut

---
+++ formulaire.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>formulaire</title>
+</head>
+<body>
+
+
+<form method="post" action="formulaire.php">
+
+	<input type="text" name="lastName">
+	<input type="text" name="firstName">
+	<input type="text" name="birthDate">
+	<input type="text" name="card">
+	<input type="text" name="cardNumber">
+
+	
+</form>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
